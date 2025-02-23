b5a2e54cada7f0a5647017dde1719aff8882841a
R0GGER <8298741+R0GGER@users.noreply.github.com>
2025-02-23 14:58:01 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Update Bookmarkly als er een nieuwe versie beschikbaar is
 /usr/local/bin/update-bookmarkly.sh
 
-# Zorg ervoor dat alle benodigde directories bestaan
 mkdir -p /var/www/html/bookmarkly/data/uploads/icons
 chown -R www-data:www-data /var/www/html/bookmarkly/
 
-# Zet de juiste rechten voor alle data directories
 find /var/www/html/bookmarkly/data -type d -exec chmod 777 {} \;
 find /var/www/html/bookmarkly/data -type f -exec chmod 666 {} \;
 
-# Start Apache in de voorgrond
 exec apache2-foreground 
